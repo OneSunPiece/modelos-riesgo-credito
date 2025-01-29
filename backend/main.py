d3885ab4c3e02f1a4077703b2cbe64a7c7c1cfb2
codeinpieces <68757620+codeinpieces@users.noreply.github.com>
2025-01-29 17:00:17 -0500

    Fix the order of columns

--- backend/main.py
+++ backend/main.py
@@ -22,51 +22,33 @@ S3_BUCKET: str = 'myawzbucket'
 MODEL_FILE_NAME:str = 'model.pkl'
 PREPROCESSOR_FILE_NAME:str = 'preprocessor.pkl'
 
-# Define column names
 COLUMN_NAMES = [
-'loan_amnt',
-'funded_amnt_inv',
-'term',
-'int_rate',
-'installment',
-'sub_grade',
-'home_ownership',
-'annual_inc',
-'verification_status',
-'pymnt_plan',
-'dti',
-'delinq_2yrs',
-'inq_last_6mths',
-'mths_since_last_delinq',
-'mths_since_last_record',
-'open_acc',
-'pub_rec',
-'revol_bal',
-'initial_list_status',
-'out_prncp_inv',
-'total_pymnt',
-'total_pymnt_inv',
-'last_pymnt_amnt',
-'mths_since_last_major_derog',
-'application_type',
-'annual_inc_joint',
-'dti_joint',
-'verification_status_joint',
-'tot_coll_amt',
-'open_acc_6m',
-'open_il_6m',
-'open_il_12m',
-'open_il_24m',
-'mths_since_rcnt_il',
-'total_bal_il',
-'il_util',
-'open_rv_12m',
-'open_rv_24m',
-'max_bal_bc',
-'all_util',
-'inq_fi',
-'total_cu_tl',
-'inq_last_12m'
+    "loan_amnt",
+    "funded_amnt",
+    "funded_amnt_inv",
+    "term",
+    "int_rate",
+    "installment",
+    "sub_grade",
+    "annual_inc",
+    "verification_status",
+    "dti",
+    "delinq_2yrs",
+    "revol_util",
+    "total_acc",
+    "out_prncp",
+    "total_rec_prncp",
+    "total_rec_int",
+    "total_rec_late_fee",
+    "recoveries",
+    "collection_recovery_fee",
+    "collections_12_mths_ex_med",
+    "policy_code",
+    "acc_now_delinq",
+    "tot_cur_bal",
+    "total_rev_hi_lim",
+    "pymnt_plan",
+    "home_ownership"
 ]
 
 INPUT_LEN:int = len(COLUMN_NAMES) # LEN of data input
@@ -138,7 +120,7 @@ def is_valid_data(input_data):
     if len(input_data) != INPUT_LEN:
         status_error = {
             'statusCode': 400,
-            'body': json.dumps({'ERROR': 'Invalid input. Provide a list of {INPUT_LEN} numbers in "data".'})
+            'body': json.dumps({'ERROR': f'Invalid input. Provide a list of {INPUT_LEN} numbers in "data".'})
         }
         return False
     else:
@@ -207,29 +189,38 @@ def lambda_handler(event, context):
     global status_error
 
     try:
-        # Parse input from the API
-        body = json.loads(event['body'])
-        input_data = body.get('data')
-        type_data = type(input_data)
-
-        # Debugging logs
-        print(input_data)
-        logging.debug(f'Input Data: {str(input_data)}')
-        logging.debug(f'Type: {type_data}')
-        logging.debug(f'Length: {input_data}')
-
+        body = event.get("body")
+        if body:
+            # Parse the JSON string
+            parsed_body = json.loads(body)
+            # Access the "features" key inside "data"
+            input_data = parsed_body.get("data")
+            # Get the type and length of the input data
+            type_data = type(input_data)
+            input_items = len(input_data)
+            # Debugging logs
+            print(f'Input Data: {str(input_data)}')
+            print(f'Type: {type_data}')
+            print(f'Length: {input_items}')
+            #logging.debug(f'Input Data: {str(input_data)}')
+            #logging.debug(f'Type: {type_data}')
+            #logging.debug(f'Length: {input_data}')
+        
         # Validate input
         if is_valid_data(input_data):
             print('Processing data...')
             # Load the model if not already loaded
-            model = load_files_from_s3()
+            load_files_from_s3()
             # Convert input to numpy array
             preprocessed_data = preprocess_input(input_data)
+            print(f'Preprocessed Data: {preprocessed_data}')
             # Make predictions
             prediction = model.predict(preprocessed_data)
+            print(f"Prediction: {prediction}")
             prediction = prediction.tolist()  # Convert numpy array to list for JSON serialization
             # Put prediction in logs
-            logging.info(f"Prediction: {prediction}")
+            print(f"Prediction List: {prediction}")
+            #logging.info(f"Prediction: {prediction}")
             # Return the prediction
             return {
                 'statusCode': 200,
@@ -242,5 +233,5 @@ def lambda_handler(event, context):
         print(f"Error: {e}")
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            'body': [json.dumps({'error': str(e)}), input_data]
         }
