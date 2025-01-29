6ffc236eb59017564e04ec2b9fad4b66fa15c888
codeinpieces <68757620+codeinpieces@users.noreply.github.com>
2025-01-29 17:01:14 -0500

    front in almost working stage

--- frontend/src/Api/Api.jsx
+++ frontend/src/Api/Api.jsx
@@ -1,17 +1,23 @@
 export async function sendPredictionRequest(features) {
-    const API_URL = "https://opilfimn4c4uwgf26nsylvb7qa0vaybz.lambda-url.us-east-1.on.aws/";
+    const API_URL = 'https://p23mvua4bs6ikuxs4us4cbfh3m0tsiue.lambda-url.us-east-1.on.aws/'
+
     try {
       const response = await fetch(API_URL, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ features }),
-      });
+        body: JSON.stringify({
+          data: features
+        }),
+        }
+      );
+
       if (!response.ok) {
         throw new Error("Failed to fetch prediction");
       }
       return await response.json();
+
     } catch (error) {
       console.error("Error:", error);
       return { prediction: "Error occurred" };
--- frontend/src/Form/Form.jsx
+++ frontend/src/Form/Form.jsx
@@ -7,17 +7,32 @@ export default function Form({ setPrediction }) {
   // Variables
   const featureNames = [
     "loan_amnt",
+    "funded_amnt",
+    "funded_amnt_inv",
     "term",
     "int_rate",
     "installment",
-    "grade",
-    "emp_length",
-    "home_ownership",
+    "sub_grade",
     "annual_inc",
     "verification_status",
-    "purpose",
-    "label"
-  ];
+    "dti",
+    "delinq_2yrs",
+    "revol_util",
+    "total_acc",
+    "out_prncp",
+    "total_rec_prncp",
+    "total_rec_int",
+    "total_rec_late_fee",
+    "recoveries",
+    "collection_recovery_fee",
+    "collections_12_mths_ex_med",
+    "policy_code",
+    "acc_now_delinq",
+    "tot_cur_bal",
+    "total_rev_hi_lim",
+    "pymnt_plan",
+    "home_ownership"
+];
 
   // props
   Form.propTypes = {
@@ -32,10 +47,12 @@ export default function Form({ setPrediction }) {
     }, {})
   );
 
+
   // Functions
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFeatures((prevFeatures) => ({
+    setFeatures(
+      (prevFeatures) => ({
       ...prevFeatures,
       [name]: value
     }));
@@ -43,7 +60,9 @@ export default function Form({ setPrediction }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const featureArray = Object.values(features).map(Number);
+    const featureArray = Object.values(features).map((value, index, array) => 
+      index < array.length - 2 ? Number(value) : value
+    );
     const result = await sendPredictionRequest(featureArray);
     setPrediction(result.prediction);
   };
@@ -53,7 +72,6 @@ export default function Form({ setPrediction }) {
       {featureNames.map((feature) => (
         <input
           key={feature}
-          type="number"
           name={feature}
           placeholder={feature.replace("_", " ")}
           value={features[feature]}
@@ -61,6 +79,7 @@ export default function Form({ setPrediction }) {
           required
         />
       ))}
+
       <button type="submit">Predict</button>
     </form>
   );
